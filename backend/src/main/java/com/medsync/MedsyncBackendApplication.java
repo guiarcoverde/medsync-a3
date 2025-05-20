7d6965d6220d31d1992426e3b3c75a7588669da8
Guilherme Arcoverde <gui.arcoverde21@gmail.com>
2025-05-20 01:34:24 -0300

    backend first commit

---
+++ backend/src/main/java/com/medsync/MedsyncBackendApplication.java
@@ -0,0 +1,13 @@
+package com.medsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedsyncBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedsyncBackendApplication.class, args);
+	}
+
+}
