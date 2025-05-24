2ec4bf5f76b2934c3c37686614a033bd308a11e8
Guilherme Arcoverde <gui.arcoverde21@gmail.com>
2025-05-24 19:50:44 -0300

    allow public access to user update endpoint

--- backend/src/main/java/com/projetoa3/medsync/infra/config/SecurityConfig.java
+++ backend/src/main/java/com/projetoa3/medsync/infra/config/SecurityConfig.java
@@ -15,7 +15,9 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable()) // Disable CSRF for simplicity (enable for production)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/users/register").permitAll() // Allow registration without auth
+                        .requestMatchers(
+                                "/users/register",
+                                "/users/update/{id}").permitAll() // Allow registration without auth
                         .anyRequest().authenticated() // All other requests need authentication
                 );
 
